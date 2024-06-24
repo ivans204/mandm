be0afe3508285c49c306063fa0c7bce942d222ab
ivans204 <ivansvrga204@gmail.com>
2024-06-25 01:13:07 +0200

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
-import "dotenv/config";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import 'dotenv/config';
 
 export default defineConfig({
     build: {
-        minify: process.env.APP_ENV === "production" ? "esbuild" : false,
-        cssMinify: process.env.APP_ENV === "production",
+        minify: process.env.APP_ENV === 'production' ? 'esbuild' : false,
+        cssMinify: process.env.APP_ENV === 'production',
     },
     plugins: [
         laravel({
-            input: ["resources/react/app.tsx"],
+            input: ['resources/react/app.tsx'],
             refresh: true,
         }),
         react(),
